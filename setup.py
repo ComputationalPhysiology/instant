0f28c2a14d06f84b1f2aeed51344a6d43965c4f9
kent-and <devnull@localhost>
2005-10-17 12:57:51 +0000

    [project @ 5213] added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Instant", version='1.0', 
+      description="Instant Inlining of C/C++ in Python", 
+      author="Magne Westlie and Kent-Andre Mardal", 
+      url="http://www.ifi.uio.no/kent-and/software/Instant/doc/Instant.html", 
+      py_modules=['src/Instant'])
+
+
