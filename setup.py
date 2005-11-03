3f30db025b88174c972541d888ddd2bcd7a2da1d
kent-and <devnull@localhost>
2005-11-03 09:02:08 +0000

    [project @ 5405]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name="Instant", version='0.2',
       description="Instant Inlining of C/C++ in Python", 
       author="Magne Westlie and Kent-Andre Mardal", 
       author_email ="magnew@simula.no, kent-and@simula.no", 
-      url="http://www.ifi.uio.no/~kent-and/software/Instant/doc/Instant.html", 
+      url="http://sourceforge.net/projects/pyinstant", 
       package_dir={'': 'src' }, 
       py_modules=['Instant'])
 
