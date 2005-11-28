e01c7f3abe7dc5ab5a758713ecc99468bf349e26
kent-and <devnull@localhost>
2005-11-28 09:43:34 +0000

    [project @ 5744]

--- Instant-0.3/doc/maketarball.sh
+++ Instant-0.3/doc/maketarball.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-files=`find  ../../Instant-0.2 -type f| grep -v svn` 
+files=`find  ../../Instant-0.3 -type f| grep -v svn` 
 #echo $files 
-tar -cf Instant-0.2.tar $files  
-gzip Instant-0.2.tar
-ls -s Instant-0.2.tar.gz
+tar -cf Instant-0.3.tar $files  
+gzip Instant-0.3.tar
+ls -s Instant-0.3.tar.gz
 
