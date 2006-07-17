b47a7c43e7aede61d13c2e64f7d8d7e048a03fca
kent-and <devnull@localhost>
2006-07-17 11:54:28 +0000

    [project @ 8916]

--- Instant-0.6/tests/test5.py
+++ Instant-0.6/tests/test5.py
@@ -1,68 +1,46 @@
 #!/usr/bin/python
 
-import Numeric as N 
-import time
-from Instant import create_extension
+from Instant import create_extension  
+import Numeric
 import sys
+import time
 
 
 c_code = """
-void func(int n1, double* array1, int n2, double* array2){
-  double a; 
+/* add function for vectors with all safety checks removed ..*/ 
+void add(int n1, double* array1, int n2, double* array2, int n3, double* array3){
   for (int i=0; i<n1; i++) {  
-    a = array1[i]; 
-    array2[i] = sin(a) + cos(a) + tan(a);  
+    array3[i] = array1[i] + array2[i]; 
   }
 }
 """
 
-create_extension(code=c_code, headers=["arrayobject.h"], cppargs='-g',
+
+create_extension(code=c_code, headers=["arrayobject.h"], cppargs='-O3',
           include_dirs=[sys.prefix + "/include/python" + sys.version[:3] + "/Numeric"],
-          init_code='import_array();', module='test5_ext', 
-          arrays = [['n1', 'array1'],['n2', 'array2']])
+          init_code='import_array();', module='test3_ext', 
+          arrays = [['n1', 'array1'],['n2', 'array2'],['n3', 'array3']])
+
+from test3_ext import add 
+a = Numeric.arange(10000000); a = Numeric.sin(a)
+b = Numeric.arange(10000000); b = Numeric.cos(b)
+c = Numeric.arange(10000000); c = Numeric.cos(c)
+d = Numeric.arange(10000000); d = Numeric.cos(d)
 
 
 
-seed = 10000000.0
-
-
-a = N.arange(seed) 
-t1 = time.time()
-b = N.sin(a) + N.cos(a) + N.tan(a)   
+t1 = time.time() 
+add(a,b,c)
 t2 = time.time()
-print "With NumPy: ", t2-t1, "seconds" 
+print 'With Instant:',t2-t1,'seconds'
 
-
-from test5_ext import func
-
-c = N.arange(seed)
-t1 = time.time()
-func(a,c)
+t1 = time.time() 
+Numeric.add(a,b,d)
 t2 = time.time()
-print "With Instant: ", t2-t1, "seconds" 
+print 'Med numpy:   ',t2-t1,'seconds'
 
-
-t1 = time.time()
-d = N.sin(a)
-d += N.cos(a)
-d += N.tan(a) 
-t2 = time.time()
-print "With NumPy inplace aritmetic: ", t2-t1, "seconds" 
-
-
-
-difference = abs(b - c) 
+difference = abs(d - c) 
 sum = reduce( lambda a,b: a+b, difference)  
 print "The difference between the arrays computed by numpy and instant is " + str(sum) 
-
-
-
-
-
-
-
-
-
-
 
 
--- Instant-0.6/tests/test6.py
+++ Instant-0.6/tests/test6.py
@@ -1,50 +1,55 @@
 #!/usr/bin/python
 
 from Instant import create_extension  
+import Numeric as N
+import sys
+import time
+
 
 
 c_code = """
-class Sum { 
-public: 
-  virtual double sum(double a, double b){
-    return a+b;
+/* add function for matrices with all safety checks removed ..*/ 
+void add(int n1, int* p1, double* array1, 
+         int n2, int* p2, double* array2, 
+         int n3, int* p3, double* array3){
+
+  for (int i=0; i<p1[0]; i++) {
+    for (int j=0; j<p1[1]; j++) {
+      *array3 = *array1 + *array2; 
+      array3++; 
+      array2++; 
+      array1++; 
+    }
   }
-};
-
-
-double use_Sum(Sum& sum, double a, double b) {  
-  return sum.sum(a,b); 
 }
-
 """
 
-create_extension(code=c_code, module='test6_ext')
+create_extension(code=c_code, headers=["arrayobject.h"], cppargs='-g',
+          include_dirs=[sys.prefix + "/include/python" 
+                       + sys.version[:3] + "/Numeric"],
+          init_code='import_array();', module='test4_ext', 
+          arrays = [['n1', 'p1', 'array1'],
+                    ['n2', 'p2', 'array2'],
+                    ['n3', 'p3', 'array3']])
 
-from test6_ext import * 
-sum = Sum()
-a = 3.7
-b = 4.8
-c = use_Sum(sum,a,b)
-print "The sum of %g and %g is %g"% (a,b,c) 
+from test4_ext import add 
+a = N.arange(4000000); a = N.sin(a); a.shape=(2000,2000)
+b = N.arange(4000000); b = N.cos(b); b.shape=(2000,2000)
+c = N.arange(4000000); c = N.cos(c); c.shape=(2000,2000)
+d = N.arange(4000000); d = N.cos(d); d.shape=(2000,2000)
 
+t1 = time.time() 
+add(a,b,c)
+t2 = time.time()
 
-class Sub(Sum): 
-  def __init__(self): 
-    Sum.__init__(self) 
-    
-  def sum(self,a,b): 
-    print "sub" 
-    return a-b; 
+t3 = time.time() 
+N.add(a,b,d)
+t4 = time.time()
 
+difference = abs(d - c) 
+sum = reduce( lambda a,b: a+b, difference)  
+print "The difference between the arrays computed by numpy and instant is " + str(sum) 
 
-
-sub = Sub()
-a = 3.7
-b = 4.8
-c = use_Sum(sub,a,b)
-print "The sub of %g and %g is %g"% (a,b,c) 
-
-
-
-
+print 'With Instant:',t2-t1,'seconds'
+print 'Med numpy:   ',t4-t3,'seconds'
 
