f51906c35aaf9f9691eaaa2a48cb675311cfd7cf
kent-and <devnull@localhost>
2006-11-30 13:28:34 +0000

    [project @ 9760] Test

--- Instant-0.9/src/Instant.py
+++ Instant-0.9/src/Instant.py
@@ -11,7 +11,6 @@ A simple example: (see test1.py)
 >>> from Instant import inline
 >>> add_func = inline(\"double add(double a, double b){ return a+b; }\")
 >>> print "The sum of 3 and 4.5 is ", add_func(3, 4.5)
-
 """
 
 
