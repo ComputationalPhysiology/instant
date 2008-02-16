d1af572dcded2d3d4d32df8f2b0d695b6ba3c78e
Kent-Andre Mardal <kent-and@simula.no>
2008-02-16 14:43:28 +0100

    added installation of instant-cache-clean script

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from os.path import join
 from distutils.core import setup
 
 setup(name="instant", version='0.9.3', 
@@ -8,6 +9,7 @@ setup(name="instant", version='0.9.3',
       author_email ="magnew@simula.no, kent-and@simula.no", 
       url="http://www.fenics.org/instant", 
       package_dir={'': 'src' }, 
+      scripts = [join("etc" , "instant-cache-clean.sh")]
       py_modules=['instant'])
 
 
