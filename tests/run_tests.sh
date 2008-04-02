1f95d2fcf12f9c633c6a3f1950907a74c5fb69f4
Kent-Andre Mardal <kent-and@simula.no>
2008-04-02 18:18:21 +0200

    updated run_tests according to Ilmar Wilbers

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 
-for file in *.py;  
-do 
-echo "running test $file ";
-python $file; 
+for file in *.py; do
+   echo "running test $file ";
+   python $file;
+   if [ $? != 0 ]; then
+     exit
+   fi
 done
 
 
