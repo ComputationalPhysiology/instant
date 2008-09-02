72bfea758ee748bdc5667eeac19067b4fa6a9e34
Martin Sandve Alnæs <martinal@simula.no>
2008-09-02 19:34:37 +0200

    Formatting fix while doing debugging printing. Tests fail because a is "Not a NumPy array", and printing type(a) shows that a is indeed a numarray array or Numeric array as it should be. Something wrong in the typemapping it seems.

--- tests/test_numarray.py
+++ tests/test_numarray.py
@@ -16,7 +16,8 @@ double sum (int n1, double* array1){
 
 sum_func = inline_with_numarray(c_code, arrays = [['n1', 'array1']], cache_dir="test_cache")
 
-a = numarray.arange(10000000); a = numarray.sin(a)
+a = numarray.arange(10000000)
+a = numarray.sin(a)
 
 t1 = time.time()
 sum1 = sum_func(a)
@@ -30,6 +31,4 @@ print 'With numarray:   ',t2-t1,'seconds'
 
 difference = abs(sum1 - sum2) 
 print "The difference between the sums computed by numarray and instant is " + str(difference) 
-
-
 
