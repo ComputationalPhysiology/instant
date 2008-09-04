067825c8eacc22c36baa6f29cd6705d06994e886
Johannes Ring <johannr@simula.no>
2008-09-04 11:25:35 +0200

    Generate batch files for Windows.

--- setup.py
+++ setup.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python
 
-from os.path import join
+import sys, platform
+from os.path import join, splitext
 from distutils.core import setup
+
+scripts = [join("etc", "instant-clean"), join("etc", "instant-showcache")]
+if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
+    # In the Windows command prompt we can't execute Python scripts 
+    # without the .py extension. A solution is to create batch files
+    # that runs the different scripts.
+
+    # try to determine the installation prefix
+    # first set up a default prefix:
+    if platform.system() == "Windows":
+        prefix = sys.prefix
+    else:
+        # we are running bdist_wininst on a non-Windows platform
+        pymajor, pyminor = sysconfig.get_python_version().split(".")
+        prefix = "C:\\Python%s%s" % (major, pyminor)
+
+    # if --prefix is specified we use this instead of the default:
+    for arg in sys.argv:
+        if "--prefix" in arg:
+            prefix = arg.split("=")[1]
+            break
+
+    # create batch files for Windows:
+    for batch_file in ["instant-clean.bat", "instant-showcache.bat"]:
+        f = open(batch_file, "w")
+        f.write("@python %s %%*" % \
+                join(prefix, "Scripts", splitext(batch_file)[0]))
+        f.close()
+        scripts.append(batch_file)
 
 setup(name = "instant", version = '0.9.5', 
       description = "Instant Inlining of C/C++ in Python", 
@@ -10,5 +40,5 @@ setup(name = "instant", version = '0.9.5',
       url = "http://www.fenics.org/instant", 
       packages = ['instant'],
       package_dir = {'instant': 'src/instant'}, 
-      scripts = [join("etc", "instant-clean"), join("etc", "instant-showcache")])
+      scripts = scripts)
 
