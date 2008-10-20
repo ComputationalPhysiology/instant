10e1121a70848c4ab11497b19db085e7d3138d22
Kent-Andre Mardal <kent-and@simula.no>
2008-10-20 14:40:15 +0200

    updated maketarball.sh script

--- etc/maketarball.sh
+++ etc/maketarball.sh
@@ -2,7 +2,7 @@
 
 
 rm -rf ../build
-rm instant-0.9.4.tar.gz
+rm instant-0.9.5.tar.gz
 
 cd ../tests 
 sh clean.sh 
@@ -10,7 +10,7 @@ sh clean.sh
 cd ../etc
 files=`find  ../../instant -type f| grep -v hg` 
 echo $files 
-tar -cf instant-0.9.4.tar $files  
-gzip instant-0.9.4.tar
-ls -s instant-0.9.4.tar.gz
+tar -cf instant-0.9.5.tar $files  
+gzip instant-0.9.5.tar
+ls -s instant-0.9.5.tar.gz
 
