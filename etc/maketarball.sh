c1d50fded94cc0aa2014baf2b7c2158934bb5f70
Kent-Andre Mardal <kent-and@simula.no>
2008-10-20 14:51:15 +0200

    yet another small fix

--- etc/maketarball.sh
+++ etc/maketarball.sh
@@ -11,9 +11,12 @@ cd ../..
 cp -r instant instant-0.9.5
 cd instant-0.9.5/etc
 
-files=`find  ../../instant -type f| grep -v hg` 
-echo $files 
+files=`find  ../../instant-0.9.5 -type f| grep -v hg` 
+#echo $files 
 tar -cf instant-0.9.5.tar $files  
 gzip instant-0.9.5.tar
 ls -s instant-0.9.5.tar.gz
+cp instant-0.9.5.tar.gz ../../instant/etc/. 
+rm -rf ../../instant-0.9.5 
+
 
