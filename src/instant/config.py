f71fc0b703bdd1106b956d027962222f032aca61
Johannes Ring <johannr@simula.no>
2009-01-12 17:13:49 +0100

    Set PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 by default.

--- src/instant/config.py
+++ src/instant/config.py
@@ -1,5 +1,6 @@
 """This module contains helper functions for configuration using pkg-config."""
 
+import os
 from output import get_status_output
 
 def header_and_libs_from_pkgconfig(*packages, **kwargs):
@@ -13,27 +14,33 @@ def header_and_libs_from_pkgconfig(*packages, **kwargs):
     if result != 0: 
         raise OSError("The pkg-config package is not installed on the system.")
 
+    env = os.environ.copy()
+    try:
+        assert env("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS") == "0"
+    except:
+        env["PKG_CONFIG_ALLOW_SYSTEM_CFLAGS"] = "1"
+
     includes = []
     flags = []
     libs = []
     libdirs = []
     linkflags = []
     for pack in packages:
-        result, output = get_status_output("pkg-config --exists %s " % pack)
+        result, output = get_status_output("pkg-config --exists %s " % pack, env=env)
         if result == 0: 
-            tmp = get_status_output("pkg-config --cflags-only-I %s " % pack)[1].split()
+            tmp = get_status_output("pkg-config --cflags-only-I %s " % pack, env=env)[1].split()
             includes.extend(i[2:] for i in tmp)
             
-            tmp = get_status_output("pkg-config --cflags-only-other %s " % pack)[1].split()
+            tmp = get_status_output("pkg-config --cflags-only-other %s " % pack, env=env)[1].split()
             flags.extend(tmp)
             
-            tmp = get_status_output("pkg-config --libs-only-l  %s " % pack)[1].split()
+            tmp = get_status_output("pkg-config --libs-only-l  %s " % pack, env=env)[1].split()
             libs.extend(i[2:] for i in tmp)
             
-            tmp = get_status_output("pkg-config --libs-only-L  %s " % pack)[1].split()
+            tmp = get_status_output("pkg-config --libs-only-L  %s " % pack, env=env)[1].split()
             libdirs.extend(i[2:] for i in tmp)
 
-            tmp = get_status_output("pkg-config --libs-only-other  %s " % pack)[1].split()
+            tmp = get_status_output("pkg-config --libs-only-other  %s " % pack, env=env)[1].split()
             linkflags.extend(tmp)
 
         else: 
