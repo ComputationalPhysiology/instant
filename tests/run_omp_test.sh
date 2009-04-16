d0cbf9d42de19649b57eead0dfffb0b9b158ade2
Ilmar Wilbers <ilmarw@simula.no>
2009-04-16 14:15:20 +0200

    Reflect file name change in test script.

--- tests/run_omp_test.sh
+++ tests/run_omp_test.sh
@@ -1,9 +1,9 @@
 
 export OMP_NUM_THREADS=1
-python test_ode_omp.py
+python _test_ode_omp.py
 export OMP_NUM_THREADS=2
-python test_ode_omp.py
+python _test_ode_omp.py
 export OMP_NUM_THREADS=3
-python test_ode_omp.py
+python _test_ode_omp.py
 export OMP_NUM_THREADS=4
-python test_ode_omp.py
+python _test_ode_omp.py
