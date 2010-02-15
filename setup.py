3d90133c2e5a6e9be66f8c7a4577d7a0ccaa0004
Anders Logg <logg@simula.no>
2010-02-15 16:32:05 +0100

    Add FEniCS release scripts and other compatibility fixes

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import sys, platform
 from os.path import join, split, pardir
 from distutils.core import setup
 
-scripts = [join("etc", "instant-clean"), join("etc", "instant-showcache")]
+scripts = [join("scripts", "instant-clean"), join("scripts", "instant-showcache")]
 
 if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
-    # In the Windows command prompt we can't execute Python scripts 
+    # In the Windows command prompt we can't execute Python scripts
     # without a .py extension. A solution is to create batch files
     # that runs the different scripts.
     batch_files = []
@@ -19,17 +19,16 @@ if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
         batch_files.append(batch_file)
     scripts.extend(batch_files)
 
-setup(name = "instant", version = '0.9.7', 
-      description = "Instant Inlining of C/C++ in Python", 
-      author = "Magne Westlie, Kent-Andre Mardal, Martin Sandve Alnes and Ilmar M. Wilbers", 
-      author_email = "kent-and@simula.no, martinal@simula.no, ilmarw@simula.no", 
-      url = "http://www.fenics.org/instant", 
+setup(name = "instant", version = '0.9.7',
+      description = "Instant Inlining of C/C++ in Python",
+      author = "Magne Westlie, Kent-Andre Mardal, Martin Sandve Alnes and Ilmar M. Wilbers",
+      author_email = "kent-and@simula.no, martinal@simula.no, ilmarw@simula.no",
+      url = "http://www.fenics.org/instant",
       packages = ['instant'],
-      package_dir = {'instant': join('src', 'instant')},
+      package_dir = {'instant': 'instant'},
       package_data = {'': [join('swig', 'numpy.i')]},
       scripts = scripts,
       data_files = [(join("share", "man", "man1"),
                      [join("doc", "man", "man1", "instant-clean.1.gz"),
                       join("doc", "man", "man1", "instant-showcache.1.gz")])]
       )
-
