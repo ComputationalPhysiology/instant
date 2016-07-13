bb1dcabc14dfa6a6214eb0db2f8ada4fcb0471c9
Garth N. Wells <gnw20@cam.ac.uk>
2016-07-13 22:38:54 +0100

    Add pipelines files.

--- test/rerun_tests.py
+++ test/rerun_tests.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python
+
 from __future__ import print_function
-import os, sys, glob
+import pytest
 
 print("Not cleaning local test cache before tests.")
-
-failed_tests = []
-for f in glob.glob("test*.py"):
-   print("Running test", f, sep=" ")
-   failure = os.system("python " + f)
-   if failure:
-       failed_tests.append(f)
-
-if failed_tests:
-    print("\nThe following tests failed:")
-    for f in failed_tests:
-        print(f)
-    sys.exit(len(failed_tests))
+pytest.main()
--- test/run_tests.py
+++ test/run_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os, sys, glob, shutil
+import os
 import pytest
 
 print("Cleaning local test cache before tests.")
