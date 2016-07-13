36a335b722c506f6559d2c2255c5c4f9fac45cd0
Garth N. Wells <gnw20@cam.ac.uk>
2016-07-13 22:32:45 +0100

    Run pytest from top level test script.

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,20 +1,11 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 from __future__ import print_function
 import os, sys, glob, shutil
+import pytest
 
 print("Cleaning local test cache before tests.")
 os.system("python clean.py")
 
-failed_tests = []
-for f in glob.glob("test*.py"):
-    print("Running test %s" % f)
-    failure = os.system("python " + f)
-    if failure:
-        failed_tests.append(f)
-
-if failed_tests:
-    print("\nThe following tests failed:")
-    for f in failed_tests:
-        print(f)
-    sys.exit(len(failed_tests))
+# Run tests
+pytest.main()
