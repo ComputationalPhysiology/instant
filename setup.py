ee07b6bbd596ea18d8ec820072dcbc1114160ad2
Johannes Ring <johannr@simula.no>
2017-05-09 14:38:36 +0200

    Update version number to 2017.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
         batch_files.append(batch_file)
     scripts.extend(batch_files)
 
-version = "2017.1.0.dev0"
+version = "2017.1.0"
 
 url = "https://bitbucket.org/fenics-project/instant/"
 tarball = None
